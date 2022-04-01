baf4fa7908c9c62057a3e17f9daaf340935a782b
Pamela S-S <atheanae@gmail.com>
2022-04-01 17:43:26 -0400

    Landing - add form

--- CompanyLanding/src/main/java/com/example/companylanding/LoginServlet.java
+++ CompanyLanding/src/main/java/com/example/companylanding/LoginServlet.java
@@ -1,6 +1,15 @@
 package com.example.companylanding;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
 }
