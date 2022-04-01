3a68a6dd7da3c86ac9f8c7c8ff0e6707f7efac76
Pamela S-S <atheanae@gmail.com>
2022-04-01 19:26:57 -0400

    SQL - create tables

---
+++ Assignment_3/jjd.sql
@@ -0,0 +1,59 @@
+/***** Table No. 1 - user ****/
+-- Primary Key: id
+        create table user
+        (
+            id int identity(1,1) not null, 
+            name text
+            constraint pk_user primary key clustered (id asc)
+        )
+        ;
+        go
+
+        create table activity
+        (
+            id int identity(1,1) not null, 
+            name text
+            constraint pk_activity primary key clustered (id asc)
+        )
+        ;
+        go
+
+        create table user_activity
+        (
+            activity_id int, -- FK (id in activity)
+            user_id int, -- FK (id in user)
+            occurrence timestamp
+            constraint pk_activity_user primary key clustered 
+            (
+                activity_id asc,
+                user_id asc
+            )
+        )
+        ;
+        go
+        
+        alter table user_activity
+	        add constraint fk_user_user_activity foreign key (user_id)
+		        references user (id)
+		;
+		
+		alter table user_activity
+	        add constraint fk_activity_user_activity foreign key (activity_id)
+		        references activity (id)
+		;
+
+
+
+        -- create table user(id int identity(1,1) not null, name text);
+        -- create table activity(id int identity(1,1) not null, name text);
+        -- create table user_activity(activity_id int, user_id int, occurrence timestamp, );
+        
+        -- alter table user_activity
+	    --     add constraint fk_user_user_activity foreign key (user_id)
+		-- references user (id)
+		-- ;
+		
+		-- alter table user_activity
+	    --     add constraint fk_activity_user_activity foreign key (activity_id)
+		-- references activity (id)
+		-- ;
